9a04efa52b89b4d0cfb9ae63c7fa21b325806502
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-06-09 15:08:03 -0300

    Effect JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,29 @@
 const msgEmail = document.querySelector('.email-sucess');
 const submmited = document.querySelector('.btn-contact');
 const date = document.querySelector('.date');
+const schollLinks = document.querySelectorAll('.scroll-link')
 
 
+
+schollLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault()
+    const id = e.target.getAttribute('href').slice(1)
+    const element = document.getElementById(id);
+    const position = element.offsetTop;
+    window.scrollTo({
+      top: position,
+      behavior: 'smooth'
+    })
+  })
+})
 
 const fillCurrentYear = () => {
   const dateNow = new Date().getFullYear()
   date.textContent = dateNow;
 }
 
+window.addEventListener('load', fillCurrentYear)
 submmited.addEventListener('click', () => {
   msgEmail.classList.add('btn-activate');
 
@@ -17,4 +32,5 @@ submmited.addEventListener('click', () => {
   }, 5000);
 });
 
-window.addEventListener('load', fillCurrentYear)
+
+
